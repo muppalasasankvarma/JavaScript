300f287948b7d6fe9bfed9985ceab2e9a84a1425
muppalasasankvarma <52321050+muppalasasankvarma@users.noreply.github.com>
2023-09-26 12:56:31 +0000

    added datatypes and modify in variable.js

---
+++ Javascript/datatypes.js
@@ -0,0 +1,10 @@
+//javascript Datatypes severl type let check below what are those;
+
+let wel=1233 /// 123 are number datatype
+let wel1="hi" //// "hi" is string - srting are discribe in " " || ' '
+let wel2=true || false /// true or false are boolean datatype
+let wel3=null /// null is data type its returns null value and its type is object
+let wel4=undefined /// undesined is datatype when its occers value is empty 
+
+
+console.log(typeof wel2);// console.log its shown => datatype
--- Javascript/variables.js
+++ Javascript/variables.js
@@ -1,4 +1,25 @@
 const welcome=123456;
  
-console.log(welcome);
+ 
+
+let welcome2=123456;
+welcome2="sasi"
+ 
+var wel=123;
+var wel="hi"
+console.log(wel);
+
+function welcomefn()
+{
+    let welcome2="update";
+    var wel="change";
+    var wel=wel3;
+     var wel3="aginchange";
+    console.log(wel3);
+}
+welcomefn();
+
+ 
+
+
 
